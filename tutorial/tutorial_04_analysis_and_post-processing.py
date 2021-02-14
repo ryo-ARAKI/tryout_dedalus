257bf93c0aa85667e1a310c585093b9be3292b00
ryo-ARAKI <r_araki@fm.me.es.osaka-u.ac.jp>
2021-02-14 18:21:09 +0100

    update tutorial_04: Post-processing

--- tutorial/tutorial_04_analysis_and_post-processing.py
+++ tutorial/tutorial_04_analysis_and_post-processing.py
@@ -4,6 +4,7 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 from dedalus import public as de
+from dedalus.tools import post
 
 # Clean up any old files
 import shutil
@@ -65,3 +66,55 @@ while solver.ok:
     solver.step(dt)
     if solver.iteration % 1000 == 0:
         print('Completed iteration {}'.format(solver.iteration))
+
+
+# File arrangement
+print(subprocess.check_output("find analysis", shell=True).decode())
+
+
+# Merging output spatially parallelized  files
+post.merge_process_files("analysis", cleanup=True)
+
+print(subprocess.check_output("find analysis", shell=True).decode())
+
+# Merging output files in both time and space
+set_paths = list(pathlib.Path("analysis").glob("analysis_s*.h5"))
+post.merge_sets("analysis/analysis.h5", set_paths, cleanup=True)
+
+print(subprocess.check_output("find analysis", shell=True).decode())
+
+# Handling data
+# Plot time series of the average magnitude
+with h5py.File("analysis/analysis.h5", mode='r') as file:
+    # Load datasets
+    mag_u = file['tasks']['<|u|>']
+    t = mag_u.dims[0]['sim_time']
+    # Plot data
+    plt.close()
+    fig = plt.figure(figsize=(6, 4), dpi=100)
+    plt.plot(t[:], mag_u[:].real)
+    plt.xlabel('t')
+    plt.ylabel('<|u|>')
+    plt.savefig("fig/Hole-defect_chaos_in_the_CGLE_u_magnitute.png")
+
+# Plot phase of the system
+with h5py.File("analysis/analysis.h5", mode='r') as file:
+    # Load datasets
+    u = file['tasks']['u']
+    t = u.dims[0]['sim_time']
+    x = u.dims[1][0]
+    # Plot data
+    u_phase = np.arctan2(u[:].imag, u[:].real)
+    plt.close()
+    fig = plt.figure(figsize=(6, 7), dpi=100)
+    plt.pcolormesh(
+        x[:], t[:], u_phase,
+        shading='nearest',
+        cmap='twilight_shifted'
+    )
+    plt.colorbar()
+    plt.xlabel('x')
+    plt.ylabel('t')
+    plt.title('Hole-defect chaos in the CGLE: phase(u)')
+    plt.tight_layout()
+    plt.savefig("fig/Hole-defect_chaos_in_the_CGLE_u_phase.png")
